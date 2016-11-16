15d129c2927456a7e4468dae71023edbdd0caf84
Sean Mikalson <sean.mikalson@gmail.com>
2016-11-15 19:22:45 -0700

    Basic, basic UI for intention input

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <intention-input></intention-input>
+  </div>
+</template>
+
+<script>
+import IntentionInput from './components/intention-input'
+
+export default {
+  name: 'app',
+  components: {
+    IntentionInput
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/intention-input.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="intention-input">
+    <div>
+      <span>Goal!</span>
+    </div>
+    <div>
+      <input class="int-input" placeholder="Need a nudge?" v-model="intention"></input>
+      <button class="btn intention-button" @click="intentionEntered">Add!</button>
+      <div>
+        {{intention}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'intention-input',
+  data () {
+    return {
+      intention: ''
+    }
+  },
+  methods: {
+    intentionEntered: function() {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.intention-input {
+  text-align: center;
+}
+
+.btn {
+  width: 50px;
+  height: 20px;
+}
+
+input {
+  padding: 5px;
+  width: 200px;
+  height: 20px;
+}
+</style>
